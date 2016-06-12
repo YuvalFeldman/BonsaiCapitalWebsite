700397e52ec3cefb7d6b40888b670b56459a06ef
YuvalFeldman <feldmanyuval@gmail.com>
2016-06-12 20:41:01 +0300

    created the mail form and some, also delete node, fuck node

---
+++ Bonsainet/js/AngTemplateController.js
@@ -0,0 +1,69 @@
+app.controller('TemplateController', function ($scope, $rootScope, $http, GlobalData) {  
+    $scope.EnglishTemplates = { 
+        index: 'Templates/En/index.html',
+        about: 'Templates/En/about.html',
+        contact: 'Templates/En/contact.html'
+    };
+    $scope.HebrewTemplates = { 
+        index: 'Templates/He/index.html',
+        about: 'Templates/He/about.html',
+        contact: 'Templates/He/contact.html'
+    };
+    
+    $scope.ChosenTemplate = $scope.EnglishTemplates;
+    $scope.Content = $scope.ChosenTemplate['index'];
+
+    $rootScope.$on("UpdateAllLanguages", function(){
+           $scope.UpdateSectionLanguage(GlobalData.GetLanguage());
+    });
+    
+    $rootScope.$on("ChangePageTemplate", function(){
+           $scope.UpdateSectionContent(GlobalData.GetPage());
+    });
+    
+    $scope.UpdateSectionContent = function(page){
+        if($scope.ChosenTemplate.hasOwnProperty(page)){
+            $scope.Content = $scope.ChosenTemplate[page];
+        }
+        else{
+            //404 + send error message to admin
+        }
+    }
+
+    $scope.UpdateSectionLanguage = function(language){
+        switch(language){
+            case 'he':
+                $scope.ChosenTemplate = $scope.HebrewTemplates;
+                $scope.Content = $scope.ChosenTemplate[GlobalData.GetPage()];
+                break;
+            case 'en':
+                $scope.ChosenTemplate = $scope.EnglishTemplates;
+                $scope.Content = $scope.ChosenTemplate[GlobalData.GetPage()];
+                break;
+        }
+    }
+    
+    $scope.InternalLink = function(link){
+        GlobalData.SetPage(link);
+        $scope.UpdateSectionLanguage(GlobalData.GetPage());
+    }
+    
+    $scope.mailContent = {name: "", mail: "", message: ""};
+    
+    $scope.mailSent = false;
+    
+    $scope.submitMail = function(){
+        
+        $scope.mailSent = true;
+        
+        $http({
+          method  : 'POST',
+          url     : '/php/contact-form-handler.php',
+          data    : $scope.mailContent,
+          headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
+         });
+    }
+
+
+    $scope.UpdateSectionLanguage(GlobalData.GetLanguage());
+});
---
+++ Bonsainet/php/contact-form-handler.php
@@ -0,0 +1,25 @@
+<!--
+	Created by Yuval Feldman
+    Website: yuvalfeldman.com \ feldmanyuval.com
+    GitHub: https://github.com/YuvalFeldman
+-->
+
+<?php 
+    $_POST = json_decode(file_get_contents('php://input'), true);
+
+    $myemail = 'feldmanyuval@gmail.com';
+
+    $name = $_POST['name']; 
+    $email_address = $_POST['mail']; 
+    $message = $_POST['message']; 
+
+	$to = $myemail; 
+	$email_subject = "Contact form submission: $name";
+	$email_body = "You have received a new message. ".
+	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
+	
+	$headers = "From: $myemail\n"; 
+	$headers .= "Reply-To: $email_address";
+	
+	mail($to,$email_subject,$email_body,$headers); 
+?>
